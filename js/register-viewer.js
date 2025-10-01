0029b14f164e068f8d495b3aa932e56eb2ee06c5
JanisPlayer <54918417+JanisPlayer@users.noreply.github.com>
2025-10-01 03:41:50 +0200

    add RAWViewer legacy component for DNG/DCRAW previews

--- js/register-viewer.js
+++ js/register-viewer.js
@@ -1,9 +1,44 @@
-	if (OCA.Viewer) {
-		OCA.Viewer.registerHandler({
-			id: 'camerarawpreviews',
-			mimesAliases: {
-				'image/x-dcraw': 'image/jpeg'
-			}
-		})
+(function (OCA) {
+	if (!OCA.Viewer) {
+		console.warn('Viewer not available')
+		return
 	}
 
+	const RAWViewer = {
+		name: 'RAWViewer',
+		props: {
+			filename: { type: String, default: null },
+			fileid: { type: Number, default: null },
+			previewPath: { type: String, default: null },
+		},
+		render(createElement) {
+			if (!this.previewPath) {
+				return createElement('div', 'Kein Preview verfügbar')
+			}
+			const url = OC.generateUrl(this.previewPath)
+			return createElement('img', {
+				attrs: {
+					src: url,
+					alt: this.filename || 'RAW preview',
+					style: 'max-width: 100%; max-height: 100%; object-fit: contain;',
+				},
+				on: {
+					load: () => {
+						this.doneLoading()
+					}
+				}
+			})
+		},
+	}
+
+	OCA.Viewer.registerHandler({
+		id: 'camerarawpreviews',
+		group: 'media',
+		mimes: [
+			'image/x-dcraw',
+		],
+		component: RAWViewer,
+	})
+
+	console.log('RAWViewer registered!')
+})(OCA)
